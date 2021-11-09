007f87493e3e35d83a8bf005a7302b8531b6c851
mac <h.makusota@outlook.com>
2021-11-09 18:12:44 +0100

    portfolio section well structured and introduction of footer

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-import data from './data.js'
 
 const porfolioImg = document.querySelector('.img-main img');
 count = 0;
 
-console.log(data.img);
+var i = 0;
+var path = new Array;
+
+path[0] = "./project1.png"
+path[1] = "./snappysoap.png"
+path[2] = "./sunnyside.png"
+console.log(path.length);
+function swapImage()
+{
+   document.slide.src = path[i];
+   if(i < path.length - 1) {
+       i++;
+    } else { i = 0;}
+
+   setTimeout("swapImage()", 5000);
+}
+
+window.onload=swapImage;
