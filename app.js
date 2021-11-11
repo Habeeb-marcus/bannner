6c21fbda57cd7c356a54b317b6b27bbe26c893ea
mac <h.makusota@outlook.com>
2021-11-11 18:34:33 +0100

    handling general responsiveness

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// import data from './data.json'
 const porfolioImg = document.querySelector('.img-main img');
 count = 0;
 
@@ -19,4 +19,39 @@ function swapImage()
    setTimeout("swapImage()", 5000);
 }
 
-window.onload=swapImage;
+
+
+// console.log(data);
+
+
+const getData =  async() => {
+const response = await fetch('./data.json');
+const data = await response.json()
+console.log(data);
+return data;
+
+
+}
+
+getData()
+.then((data) => {
+   // (console.log('resolved', data))
+   changeReview();
+})
+.catch(err => ('rejected', err));
+
+
+const changeReview = () => {
+   const name = document.getElementById('name');
+   const job = document.getElementById('job');
+   const comment = document.getElementById('comment');
+
+   name.innerHTML = data[i];
+   if(i < path.length - 1) {
+       i++;
+    } else { i = 0;}
+
+   setTimeout("swapImage()", 5000);
+}
+
+window.onload=swapImage(), changeReview();
